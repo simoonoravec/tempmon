7fbfeafe496a9980beba59532517102588e3d57c
root <root@simonberry>
2023-08-17 15:49:42 +0200

    Update storage from JSON to SQLITE

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const ws = require("ws");
 const BME280 = require("bme280-sensor");
 const cron = require("node-schedule");
-const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
-const dataFile = "./data.json";
+const sqlite = require("sqlite3");
+const db = new sqlite.Database('./data.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to database');
+
+  db.exec(`CREATE TABLE IF NOT EXISTS data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time INTEGER,
+    temp DOUBLE,
+    humidity DOUBLE,
+    pressure DOUBLE
+  );`);
+});
 
 const bme280 = new BME280({
   i2cBusNo: 1,
@@ -13,6 +27,10 @@ const bme280 = new BME280({
 
 let wss = null;
 
+/**
+ * Read the current data from the sensor
+ * @param {*} callback 
+ */
 function readSensorData(callback) {
   bme280.readSensorData()
     .then((data) => {
@@ -24,44 +42,53 @@ function readSensorData(callback) {
     });
 }
 
-function getLongtermData() {
-  if (!fs.existsSync(dataFile)) {
-    return null;
-  }
+/**
+ * Get the logged data from database from the last N hours
+ * @returns Array
+ */
+function getLongtermData(range) {
+  let x = unixTime() - (range * 3600);
 
-  return JSON.parse(fs.readFileSync(dataFile));
-}
-
-function readToFile() {
-  readSensorData(function (data) {
-    if (data != null) {
-      var data_out = {
-        time: unixTime(),
-        temp: round(data.temperature_C),
-        humidity: round(data.humidity),
-        pressure: round(data.pressure_hPa),
-      };
-
-      if (fs.existsSync(dataFile)) {
-        var json = JSON.parse(fs.readFileSync(dataFile));
-        json.push(data_out);
-
-        json = arrayCleanup(json);
-        fs.writeFileSync(dataFile, JSON.stringify(json));
-      } else {
-        var array_out = [];
-        array_out.push(data_out);
-        fs.writeFileSync(dataFile, JSON.stringify(array_out));
+  return new Promise((resolve) => {
+    db.all(`SELECT * FROM data WHERE time > ${x}`, (err, rows) => {
+      if (err) {
+        resolve(null);
       }
-    }
+  
+      resolve(rows);
+    });
   });
 }
 
+/**
+ * Log sensor data to database
+ */
+function logData() {
+  let t = unixTime() - 259200;
+  db.run("DELETE FROM data WHERE time < ?", [t]);
+
+  readSensorData(function (data) {
+    if (data == null) {
+      return;
+    }
+
+    let time =  unixTime();
+    let temp = round(data.temperature_C);
+    let humidity = round(data.humidity);
+    let pressure = round(data.pressure_hPa);
+
+    db.run("INSERT INTO data (time, temp, humidity, pressure) VALUES (?, ?, ?, ?)", [time, temp, humidity, pressure]);
+  });
+}
+
+/**
+ * Initialize BME280 sensor
+ */
 bme280.init()
   .then(() => {
     console.log("BME280 initialization succeeded");
     cron.scheduleJob("*/5 * * * *", function () {
-      readToFile();
+      logData();
     });
     initHttp();
     initWS();
@@ -88,25 +115,16 @@ bme280.init()
   })
   .catch((err) => console.error(`BME280 initialization failed: ${err} `));
 
-function round(num) {
-  return Math.round((num + Number.EPSILON) * 100) / 100;
-}
-
-function unixTime() {
-  return Math.floor(+new Date() / 1000);
-}
-
-function arrayCleanup(arr) {
-  if (arr.length > 600) {
-    return arr.slice(-600);
-  } else {
-    return arr;
-  }
-}
-
+/**
+ * Initialize HTTP API server
+ */
 function initHttp() {
-  const server = http.createServer((req, res) => {
-    if (req.url == "/status") {
+  const server = http.createServer(async (req, res) => {
+    let parsed_url = url.parse(req.url, true);
+    let path = parsed_url.pathname;
+    let params = parsed_url.query;
+
+    if (path == "/status") {
       res.setHeader("Content-Type", "text/plain");
       readSensorData(function (data) {
         if (data == null) {
@@ -120,7 +138,7 @@ function initHttp() {
       return;
     }
 
-    if (req.url == "/data/now") {
+    if (path == "/data/now") {
       res.setHeader("Content-Type", "application/json");
       readSensorData(function (data) {
         if (data == null) {
@@ -150,9 +168,16 @@ function initHttp() {
       return;
     }
 
-    if (req.url == "/data/longterm") {
+    if (path == "/data/longterm") {
+      let range;
+      if (isNaN(params.range)) {
+        range = 24;
+      } else {
+        range = Math.round(parseInt(params.range));
+      }
+
       res.setHeader("Content-Type", "application/json");
-      var data = getLongtermData();
+      var data = await getLongtermData(range);
       if (data == null) {
         res.statusCode = 500;
         res.end(
@@ -177,6 +202,9 @@ function initHttp() {
   });
 }
 
+/**
+ * Initialize WebSocket server
+ */
 function initWS() {
   wss = new ws.WebSocketServer({ port: 65070 });
   wss.on("connection", function connection(ws) {
@@ -194,4 +222,15 @@ function initWS() {
   });
 
   console.log(`WebSocket server running at port 65070`);
+}
+
+/**
+ * Helping functions
+ */
+function round(num) {
+  return Math.round((num + Number.EPSILON) * 100) / 100;
+}
+
+function unixTime() {
+  return Math.floor(new Date() / 1000);
 }
